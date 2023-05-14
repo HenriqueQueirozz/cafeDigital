3425b22d074265ac7ce1de831c7d073b8b481d4f
PabloHenrique <pablohenriquenadai@gmail.com>
2023-05-14 12:10:21 -0300

    2023 05 14 - TransiçõesCadastro Concluídas

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
-console.log("Olá!");
+var btnEnviar = document.getElementById("main-enviarCadastro");
+var btnContinuar = document.getElementById("main-continuarCadastro");
+var btnFinalizar = document.getElementById("main-finalizarCadastro");
+var container1 = document.getElementById("cadastro_part1");
+var container2 = document.getElementById("cadastro_part2");
+var container3 = document.getElementById("cadastro_part3");
 
-var BtnContinuar = document.getElementById("main-enviarCadastro-1");
-var Cadastro1 = document.getElementsByClassName("main-second-content");
+function Enviar(){
+    container1.classList.add("inactive"); 
+    container2.classList.remove("inactive"); container2.classList.add("active");
+    console.log(container1);
+}
 
-BtnContinuar.addEventListener("click", function(){
-    Cadastro1.classList.add("inactive");
-    alert('Oi');
-})
+function Continuar(){
+    container2.classList.remove("active"); container2.classList.add("inactive");
+    container3.classList.remove("inactive"); container3.classList.add("active");
+    console.log(container1);
+}
+
+function Finalizar(){
+    window.location.href = "../manutencao.html";
+}
