6ef2cacae3413694498888d189846c1940a688ec
PabloHenrique <pablohenriquenadai@gmail.com>
2023-07-02 20:57:30 -0300

    2023 07 02 - Criação PáginaErros

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -2,7 +2,7 @@
 <div class='c'>
     <div class='_erro'>401</div>
     <hr>
-    <div class='_1'>ESSA PÁGINA</div>
-    <div class='_2'>NÃO FOI ENCONTRADA</div>
+    <div class='_1'>ESSE ACESSO</div>
+    <div class='_2'>NÃO FOI AUTORIZADO</div>
     <a class='btn' href='#'>VOLTAR PARA HOME</a>
 </div>
