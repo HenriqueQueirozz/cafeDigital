d83167599ebbee95b6724878fc0942acd4a00839
PabloHenrique <pablohenriquenadai@gmail.com>
2023-07-02 20:42:22 -0300

    2023 07 02 - AjustesResponsividade + 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Página não encontrada</title>
+        <link rel="stylesheet" href="css/erros.css">
+    </head>
+    <body>
+        <div id="clouds">
+            <div class="cloud x1"></div>
+            <div class="cloud x1_5"></div>
+            <div class="cloud x2"></div>
+            <div class="cloud x3"></div>
+            <div class="cloud x4"></div>
+            <div class="cloud x5"></div>
+        </div>
+        <div class='c'>
+            <div class='_404'>404</div>
+            <hr>
+            <div class='_1'>ESSA PÁGINA</div>
+            <div class='_2'>NÃO FOI ENCONTRADA</div>
+            <a class='btn' href='#'>VOLTAR PARA HOME</a>
+        </div>
+    </body>
+</html>
