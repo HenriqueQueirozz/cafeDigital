36a2ce4978b2d1c4729814c5e3dc7cce9b436ed5
Henrique Queiroz <henrique.q.paula@gmail.com>
2023-07-04 22:57:14 -0300

    [Debug] - vite tailwind

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,24 +2,7 @@
 <html lang="pt-br">
     <head>
         <title>Café Digital</title>
-        <!-- Metas -->
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <!-- CSS -->   
-        <link rel="stylesheet" href="./css/style.css">
-
-        <!-- <link rel="stylesheet" href="./css/choice-plan.css"> 
-        <link rel="stylesheet" href="./css/layout_cadastro.css"> -->
-
-
-        <!-- Tailwind -->
-        <script src="https://cdn.tailwindcss.com"></script>
-        @vite('resources/css/app.css')
-
-        <!-- Ícones bootstrap --> 
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+        @include('configuracoes_head')
         <link rel="stylesheet" href="./css/landing.css">
     </head>
     <body class="font-body">
