ce9c177c5a35e8c7e4bee103df2fcc0efe6ae9df
Henrique Queiroz <henrique.q.paula@gmail.com>
2023-07-04 22:54:24 -0300

    [Debug] - configuracoes_head index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,24 @@
 <html lang="pt-br">
     <head>
         <title>Café Digital</title>
-        @include('configuracoes_head')
+        <!-- Metas -->
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- CSS -->   
+        <link rel="stylesheet" href="./css/style.css">
+
+        <!-- <link rel="stylesheet" href="./css/choice-plan.css"> 
+        <link rel="stylesheet" href="./css/layout_cadastro.css"> -->
+
+
+        <!-- Tailwind -->
+        <script src="https://cdn.tailwindcss.com"></script>
+        @vite('resources/css/app.css')
+
+        <!-- Ícones bootstrap --> 
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
         <link rel="stylesheet" href="./css/landing.css">
     </head>
     <body class="font-body">
