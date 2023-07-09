636e01db3df366a20d1bceef1e5c07b5170de2bb
Henrique Queiroz <henrique.q.paula@gmail.com>
2023-07-09 19:20:56 -0300

    [Deploy] - tailwind

--- resources/views/configuracoes_head.blade.php
+++ resources/views/configuracoes_head.blade.php
@@ -8,6 +8,7 @@
 
 <!-- Tailwind -->
 <script src="https://cdn.tailwindcss.com"></script>
+@vite('resources/css/app.css')
 
 <!-- Ícones bootstrap --> 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
